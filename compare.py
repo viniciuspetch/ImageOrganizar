47927c7b4c1f2f12609ff8386f60342df4b112d5
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2019-01-08 20:08:09 -0300

    Bunch of modificatios, don't save images on memory anymore

--- compare.py
+++ compare.py
@@ -1,28 +1,20 @@
+from datetime import datetime
 import os
 import hashlib
 from PIL import Image
 
-# ImageData class
-# Stores image name, image full path, hash, 
-class ImageData:
-	def __init__(self, img, name):
-		self.img = img
-		self.name = name
-		self.bytes = None		
-		self.hash = None
-		self.size = None
-	def printall(self):
-		print(self.name, self.hash)
-	def getHash(self):
-		if hash == None:
-			self.hash = hashlib.md5(getBytes(self)).hexdigest()
-		return self.hash
-	def getBytes(self):
-		if bytes == Nome:
-			self.bytes = img.tobytes()
-		return self.bytes
-
-		
+class ImageInfo:
+	def __init__(self, img, imgname):
+		self.name = img.filename
+		self.name2 = imgname
+		self.format = img.format
+		self.size = img.size
+		self.mode = img.mode
+		self.hash = hashlib.md5(img.tobytes()).hexdigest()
+		self.histogram = img.histogram()
+	def getInfo(self):
+		print(self.name, self.size, self.format, self.mode, self.hash)
+	
 # Configuration class
 # Stores config stuff
 class Configuration:
@@ -34,45 +26,55 @@ class Configuration:
 		useDimension = True
 	def getConfig(self):
 		f = open('config.txt', 'r')
-		directory = f.readline()
+		self.directory = f.readline()
 		f.close()
-		return directory
-			
 
 # Main Code
 config = Configuration()
-dir = config.getConfig()
+config.getConfig()
+dir = config.directory
 print(dir)
-imglist = os.listdir(dir)
-iclist = []
-for imgname in imglist:
-	img = Image.open(dir+"\\"+imgname)
-	print(imgname, end=' ')
-	#print(type(img))
-	imghash = hashlib.md5(img.tobytes()).hexdigest()
-	
-	# Create ImageData object
-	imgobj = ImageData(img, imgname)	
-	imgobj.size = img.size
-	print(img.size)
-	iclist.append(imgobj)
-	#imgobj.printall()
+tmp = "D:\workspace\ImageOrganizar"
+imgdir = os.listdir(dir)
+imglist = []
 
-	for i in range(len(iclist)-2, 0, -1):
-		if imgobj.size == iclist[i].size:
-			if imgobj.hash == iclist[i].hash:
-				print(imgobj.name, iclist[i].name)
-				print(imgobj.size)
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
-					
+result = open("colision.txt", "w")
+
+for imgname in imgdir:
+	# Get image from directory
+	img = Image.open(dir+"\\"+imgname)
+	# Save image as ImageInfo
+	currimg = ImageInfo(img, imgname)
+	#currimg.getInfo()
+	imglist.append(currimg)
+	# Compare to previous images
+	for i in range(len(imglist)-2, 0, -1):
+		if currimg.hash == imglist[i].hash:
+			if currimg.size == imglist[i].size:
+				if currimg.histogram == imglist[i].histogram:
+					#print(currimg.name, imglist[i].name)
+					#result.write(currimg.name+" "+imglist[i].name+"\n")
+					if tmp is not None and os.path.isdir(tmp):
+						if not os.path.isdir(tmp+"\\"+currimg.hash):
+							os.mkdir(tmp+"\\"+currimg.hash)
+							print("[LOG "+str(datetime.now())+"]: "+tmp+"\\"+currimg.hash+" created.")
+						else:
+							print("[LOG "+str(datetime.now())+"]: directory already exists")
+						if os.path.exists(tmp+"\\"+currimg.hash):
+							if os.path.exists(currimg.name):
+								try:
+									os.rename(currimg.name, tmp+"\\"+currimg.hash+"\\"+currimg.name2)
+								except:
+									print("[ERR "+str(datetime.now())+"]: currimg renaming")
+							if os.path.exists(imglist[i].name):
+								try:
+									os.rename(imglist[i].name, tmp+"\\"+currimg.hash+"\\"+imglist[i].name2)
+									del imglist[i]
+									i = i-1
+								except:
+									print("[ERR "+str(datetime.now())+"]: imglist renaming")
+								
+			
+# Close result file and print "end"
+result.close()
+print("end")
